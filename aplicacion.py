d26c275f1599560d1fc1b66f87a7a52d8b8c1693
Pablo <pablosobral17@gmail.com>
2021-05-06 14:52:54 +0200

    Subo la feature 4

--- aplicacion.py
+++ aplicacion.py
@@ -1,1 +1,8 @@
+texto = input("Ingrese una palabra: ").lower()
+inverso = texto[::-1]
+if texto == inverso:
+    print("La palabra ingresada si es palindromo")
+else:
+    print("La palabra ingresada no es palindromo")
+
 
