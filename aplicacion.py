861a8796dc1b2cdec40b9e3fa89382e07cdbe098
Gunter895 <miriam.negrito@gmail.com>
2021-05-06 14:55:21 +0200

    Sumar 4 números

--- aplicacion.py
+++ aplicacion.py
@@ -1,1 +1,8 @@
+n1 = int(input('Introduce el primer número: '))
+n2 = int(input('Indroduce el segundo número: '))
+n3 = int(input('Introduce el tercer número: '))
+n4 = int(input('Introduce el cuarto número: '))
 
+suma = n1 + n2 + n3 + n4
+
+print('La suma es: ', suma)
